29f6bce2ff5c0a4c880fc8879adbafb35511e4f2
Marc Benito Castignani <castignanimarc@gmail.com>
2025-08-31 14:25:46 +0200

    Fix Post model to allow cascade delete of comments

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ class Post(db.Model):
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     likes = db.Column(db.Integer, default=0)
-    comments = db.relationship('Comment', backref='post', lazy=True)
+    comments = db.relationship('Comment', backref='post', lazy=True, cascade="all, delete-orphan")
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
